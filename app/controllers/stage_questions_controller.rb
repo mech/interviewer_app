d3dd26ef50ac7b242b3757cc8f3c1c16eaa82d7b
mech <mech@me.com>
2011-01-04 11:26:09 +0800

    Refactor StageQuestionsController

--- app/controllers/stage_questions_controller.rb
+++ app/controllers/stage_questions_controller.rb
@@ -3,50 +3,40 @@ class StageQuestionsController < ApplicationController
 
   before_filter :find_position
   before_filter :find_stage
+  before_filter :find_stage_question, :only => [:update, :destroy]
 
   def index
-    if @stage
-      @stage_questions = @stage.stage_questions
+    @stage_questions = @stage.stage_questions
 
-      if request.delete?
-        @stage_questions.delete_all
-        respond_with [@position, @stage]
-      end
+    if request.delete?
+      @stage_questions.delete_all
+      respond_with [@position, @stage]
     end
   end
 
   def create
-    if @stage
-      @index = params[:stage_question][:index]
-      @stage_question = @stage.stage_questions.build(params[:stage_question])
-      @stage_question.save
-      respond_with @stage_question
-    end
+    @index = params[:stage_question][:index]
+    @stage_question = @stage.stage_questions.build(params[:stage_question])
+    @stage_question.save
+    respond_with @stage_question
   end
 
   def update
-    if @stage
-      @stage_question = @stage.stage_question_at(params[:id])
-      @stage_question.update_attributes(params[:stage_question])
-      respond_with @stage_question
-    end
+    @stage_question.update_attributes(params[:stage_question])
+    respond_with @stage_question
+  end
+
+  def destroy
+    # TODO - Check if we can delete question that is being used
+    @stage_question.destroy
+    @stage_question = @stage.stage_questions.build unless @stage.locked? # ready for entry
   end
 
   def sort
     @ordering_layout = params[:qn] # => ["2", "1", "3"]
     @stage.sort_questions(@ordering_layout)
-    
-    @stage_question = @stage.stage_questions.build unless @stage.locked? # ready for entry
-  end
 
-  def destroy
-    # TODO - Check if we can delete question that is being used
-    if @stage
-      @stage_question = @stage.stage_question_at(params[:id])
-      @stage_question.destroy
-      
-      @stage_question = @stage.stage_questions.build unless @stage.locked? # ready for entry
-    end
+    @stage_question = @stage.stage_questions.build unless @stage.locked? # ready for entry
   end
 
   private
@@ -58,5 +48,9 @@ class StageQuestionsController < ApplicationController
 
   def find_stage
     @stage = @position.stage_at(params[:stage_id]) if @position
+  end
+
+  def find_stage_question
+    @stage_question = @stage.stage_question_at(params[:id]) if @stage
   end
 end
